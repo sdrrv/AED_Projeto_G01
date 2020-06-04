3b2ac57282354176715dad2c7f7d7d4215c56dce
Duarte Elvas <duartecelvas@gmail.com>
2020-06-04 17:23:50 +0100

    Familia Class Created and Updated respective tad

--- models/tad_familia.py
+++ models/tad_familia.py
@@ -16,15 +16,15 @@ class Familia(ABC):
     @abstractmethod
     def add_member(self,utente):
         pass
-
+    #Remove a member from the familly
     @abstractmethod
     def remove_member(self):
         pass
-
+    #Retruns a List with all the cuidados
     @abstractmethod
     def get_fila_de_cuidados(self):
         pass
-
+    #adds a ciudado to the cuidado list
     @abstractmethod
     def add_to_fila_de_ciudados(self,cuidado):
         pass
