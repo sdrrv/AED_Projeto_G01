14a6c34ab3fc2d14f5f2952a65643e9c58401c19
Duarte Elvas <duartecelvas@gmail.com>
2020-06-04 18:04:08 +0100

    Added Tasks to help push and pull to master

---
+++ tasks.py
@@ -0,0 +1,32 @@
+from invoke import task
+import re
+
+@task
+def test(c):
+    c.run("coverage run -m unittest discover")
+
+@task(test)
+def cov(c):
+    c.run("coverage report")
+    c.run("coverage html")
+
+@task
+def merge(c,branch):
+    c.run("git checkout master")
+    c.run("git pull")
+    c.run("git checkout "+branch)
+    c.run("git merge master")
+    c.run("git checkout "+branch)
+@task
+def push(c,branch):
+    sure= input("Are you sure you want to push to master?(y/n): ")
+    if sure=="y":
+        c.run("git checkout "+branch)
+        c.run("git push")
+        c.run("git checkout master")
+        c.run("git merge "+branch)
+        c.run("git push")
+        c.run("git checkout "+branch)
+
+    else:
+        exit()
