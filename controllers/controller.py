ec4bb394457185e36b3d7aba4ba423deb0d4ec41
Duarte Elvas <duartecelvas@gmail.com>
2020-06-06 10:37:05 +0100

    Added dict, to the controller

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,6 +1,13 @@
+from models.utente import Utente
+from models.profissional import Profissional
+from models.familia import Familia
+from models.cuidados import Cuidados
+from aed_ds.lists.singly_linked_list import SinglyLinkedList
+from aed_ds.dictionaries.hash_table import HashTable
+
 class Controller:
     def __init__(self):
-        # self.utentes = dicionario
-        # self.profissionais = dicionario
-        # self.familias = dicionario
+        self.utentes = HashTable()
+        self.profissionais = HashTable()
+        self.familias = HashTable()
         pass
