18ec885607f8cb711a71e2264561483a1e6ed0d6
Duarte Elvas <duartecelvas@gmail.com>
2020-06-06 19:20:16 +0100

    Completed the profissional class(with bugs, review needed for the remove and add cuidados), and the family class, with the same bugs

--- models/profissional.py
+++ models/profissional.py
@@ -5,18 +5,18 @@ from aed_ds.lists.singly_linked_list import SinglyLinkedList
 class Profissional(Profissional,Pessoa):
     def __init__(self, nome, categoria):
         Pessoa.__init__(self,nome)
-        self.get_categoria=categoria
+        self.categoria=categoria
         self.lista_de_cuidados = SinglyLinkedList()
     
     def get_categoria(self):
-        return self.get_categoria
+        return self.categoria
     
     def add_to_cuidados(self, cuidado):
-        pass
+        self.lista_de_cuidados.insert_last(cuidado)
 
-    def remove_from_cuidados(self, cuidado):
-        pass
+    def remove_from_cuidados(self, cuidado): # Duvida, quando removemos um cuidado, temos de remover todos os cuidados adicionados ao mesmo tempo?
+        self.lista_de_cuidados.remove_last()
 
     def get_cuidados(self):
-        pass
+        return self.lista_de_cuidados
     
