dee0eb0141281774e952a9e1e25144a261dded40
monkey <m.b.rosario@hotmail.com>
2020-06-27 18:53:05 +0100

    just saving

--- controllers/controller.py
+++ controllers/controller.py
@@ -8,116 +8,124 @@ from aed_ds.dictionaries.hash_table import HashTable
 
 class Controller:
     def __init__(self):
-        #------------Dicionário-Geral------------
+        # ------------Dicionário-Geral------------
         self.utentes = HashTable()
         self.profissionais = HashTable()
         self.familias = HashTable()
-        #----------------------------------------
+        # ----------------------------------------
         self.categorias = HashTable()
         self.faixasetarias = HashTable()
         self.serviços = HashTable()
-        #------------------categorias-----------------
-        self.categorias.insert("Medicina",HashTable())
-        self.categorias.insert("Enfermagem",HashTable())
-        self.categorias.insert("Auxiliar",HashTable())
-        #-------------------Faixas-Etárias-------------
-        self.faixasetarias.insert("Medicina",SinglyLinkedList())
-        self.faixasetarias.insert("Enfermagem",SinglyLinkedList())
-        self.faixasetarias.insert("Auxiliar",SinglyLinkedList())
-        #----------------------Serviços-----------------------------
-        #Temos de decidir como implementar os serviços.
+        # ------------------categorias-----------------
+        self.categorias.insert("Medicina", HashTable())
+        self.categorias.insert("Enfermagem", HashTable())
+        self.categorias.insert("Auxiliar", HashTable())
+        # -------------------Faixas-Etárias-------------
+        self.faixasetarias.insert("Medicina", SinglyLinkedList())
+        self.faixasetarias.insert("Enfermagem", SinglyLinkedList())
+        self.faixasetarias.insert("Auxiliar", SinglyLinkedList())
+        # ----------------------Serviços-----------------------------
+        # Temos de decidir como implementar os serviços.
 
-    #--------------------------------Checks-------------------------------------------
-    def has_professional(self,nome): #returns true if there´s a professional with that name
+    # --------------------------------Checks-------------------------------------------
+    # returns true if there´s a professional with that name
+    def has_professional(self, nome):
         return self.profissionais.has_key(nome)
 
-    def has_categoria(self,categoria): #returns true if there´s a categorua with that name
+    # returns true if there´s a categorua with that name
+    def has_categoria(self, categoria):
         return self.categorias.has_key(categoria)
 
-    def has_utente(self,nome): #returns true if there´s a utente with that name
+    def has_utente(self, nome):  # returns true if there´s a utente with that name
         return self.utentes.has_key(nome)
 
-    def has_familia(self,NomeFamilia): #returns true if there´s a familia with that name
+    def has_familia(self, NomeFamilia):  # returns true if there´s a familia with that name
         return self.familias.has_key(NomeFamilia)
-    
-    def has_faixa_etaria(self,FaixaEtaria):#returns true if there´s a Faixa Etária with that name
+
+    # returns true if there´s a Faixa Etária with that name
+    def has_faixa_etaria(self, FaixaEtaria):
         return self.faixasetarias.has_key(FaixaEtaria)
-    
-    def has_serviço(self,servico): #returns true if there´s a serviço with that name
+
+    def has_serviço(self, servico):  # returns true if there´s a serviço with that name
         return self.serviços.has_key(servico)
-    
-    def has_utente_a_familia(self,nome): #Returns true if the utente is in a familia
-        utente= self.utentes.get(nome)
+
+    # Returns true if the utente is in a familia
+    def has_utente_a_familia(self, nome):
+        utente = self.utentes.get(nome)
         return utente.has_familia()
-    
-    def has_profissionais(self): # returns true if there´s at least one professional
+
+    def has_profissionais(self):  # returns true if there´s at least one professional
         return self.profissionais.size() != 0
-    
-    def has_utentes(self): # returns true if there´s at least one utente
+
+    def has_utentes(self):  # returns true if there´s at least one utente
         return self.utentes.size() != 0
-    
-    def has_familias(self): # returns true if there´s at least one utente
+
+    def has_familias(self):  # returns true if there´s at least one utente
         return self.familias.size() != 0
 
-    def has_utente_any_cuidados(self,nome): #returns true if there´s at least one cuidado in utente
-        utente= self.utentes.get(nome)
+    # returns true if there´s at least one cuidado in utente
+    def has_utente_any_cuidados(self, nome):
+        utente = self.utentes.get(nome)
         return utente.has_cuidados()
-    
-    def has_profissional_any_cuidados(self,nome): #returns true if there´s at least one cuidado in profissional
-        profissional= self.profissionais.get(nome)
+
+    # returns true if there´s at least one cuidado in profissional
+    def has_profissional_any_cuidados(self, nome):
+        profissional = self.profissionais.get(nome)
         return profissional.has_cuidados()
-    
-    def has_familia_any_cuidados(self,NomFamilia):#returns true if there´s at least one cuidado in utente in the familia
+
+    # returns true if there´s at least one cuidado in utente in the familia
+    def has_familia_any_cuidados(self, NomFamilia):
         pass
 
-    #---------------------------------------------------------------------------------
-    def registar_profissional(self,categoria,name):
-        self.profissionais.insert(name, Profissional(name,categoria))
-    
-    def registar_utente(self,name,FaixaEtaria):
+    # ---------------------------------------------------------------------------------
+    def registar_profissional(self, categoria, name):
+        self.profissionais.insert(name, Profissional(name, categoria))
+
+    def registar_utente(self, name, FaixaEtaria):
         self.utentes.insert(name, Utente(name, FaixaEtaria))
 
-    def registar_familia(self,NomeFamilia):
+    def registar_familia(self, NomeFamilia):
         self.familias.insert(NomeFamilia, Familia(NomeFamilia))
-    
-    def associar_utente_a_familia(self,nome,NomeFamilia):
+
+    def associar_utente_a_familia(self, nome, NomeFamilia):
         familia = self.familias.get(NomeFamilia)
-        Utente=self.utentes.get(nome)
+        Utente = self.utentes.get(nome)
         familia.add_member(Utente)
         Utente.set_familia(NomeFamilia)
-        
-    def desassociar_utente_a_familia(self,nome):
+
+    def desassociar_utente_a_familia(self, nome):
         utente = self.utentes.get(nome)
         familia = self.familias.get(utente.get_familia())
         utente.remove_familia()
-        familia.remove_member(nome)   
+        familia.remove_member(nome)
 
     def listar_profissionais(self):
-        pass #Returns a List [["Medicina","Gilinho"]]
+        pass  # Returns a List [["Medicina","Gilinho"]]
 
     def listar_utentes(self):
-        pass #Returns a List[["Gil","Idoso","Gilinho"]]
+        pass  # Returns a List[["Gil","Idoso","Gilinho"]]
 
     def listar_familias(self):
-        pass #Returns a list ["Gil","Rosario"]
-    
-    def mostrar_familia(self,NomeFamilia):
-        pass #Retuns a list [["Jovem","Galinha"],["Idoso","Gilinho"]]
+        pass  # Returns a list ["Gil","Rosario"]
 
-    def marcar_cuidados_a_utente(self,nome,servico,categoria,NomeProfissional):
-        pass
-    
-    def cancelar_cuidados_marcados_a_utente(self,nome):
+    def mostrar_familia(self, NomeFamilia):
+        pass  # Retuns a list [["Jovem","Galinha"],["Idoso","Gilinho"]]
+
+    def marcar_cuidados_a_utente(self, nome, servico, categoria, NomeProfissional):
         pass
 
-    def listar_cuidados_marcados_a_utente(self,nome):
-        pass #Returns a list with objects cuidados
+    def cancelar_cuidados_marcados_a_utente(self, nome):
+        pass
 
-    def listar_cuidados_marcados_a_familia(self,NomeFamilia):
-        pass # Returns a list [["Dudas","Consulta","Medicina","Gilinho"]]
+    def listar_cuidados_marcados_a_utente(self, nome):
+        pass  # Returns a list with objects cuidados
 
-    def listar_servicos_marcados_a_profissional(self,Categoria,NomeProfissional):
-        pass # returns a list [["Consulta","Dudas"]]
+    def listar_cuidados_marcados_a_familia(self, NomeFamilia):
+        pass  # Returns a list [["Dudas","Consulta","Medicina","Gilinho"]]
 
-    def listar_marcações_por_tipo_de_servico(self,servico):
-        pass #Returns a list [["Medicina","Gilinho","Dudas"]]
+    def listar_servicos_marcados_a_profissional(self, Categoria, NomeProfissional):
+        pass  # returns a list [["Consulta","Dudas"]]
+
+    def listar_marcacoes_por_tipo_de_servico(self, servico):
+        pass  # Returns a list [["Medicina","Gilinho","Dudas"]]
+
--- views/cli.py
+++ views/cli.py
@@ -1,104 +1,136 @@
 from controllers.controller import Controller
 
 
-def CLI():
-    controller = Controller()
+class CLI():
+    def __init__(self):
+        self.controller = Controller()
 
-    while True:
-        line = input()
-        if line == "":
-            exit()
-        commands = line.split()
+        while True:
+            line = input()
+            if line == "":
+                exit()
+            commands = line.split()
 
-        # Resgistar Profissional-----------------------"RP Categoria Nome"
-        if (commands[0] == "RP"):
-            if controller.has_professional(commands[2]):
-                print("Profissional existente.")
+            # Resgistar Profissional-----------------------"RP Categoria Nome"
+            if (commands[0] == "RP"):
+                if self.controller.has_professional(commands[2]):
+                    print("Profissional existente.")
 
-            elif not controller.has_categoria(commands[1]):
-                print("Categoria inexistente.")
+                elif not self.controller.has_categoria(commands[1]):
+                    print("Categoria inexistente.")
 
+                else:
+                    self.controller.registar_profissional(commands[2])
+                    print("Profissional registado com sucesso.")
+
+            # Resgistar Utente------------------------------"RU Nome FaixaEtária"
+            elif (commands[0] == "RU"):
+
+                if self.controller.has_utente(commands[1]):
+                    print("Utente existente.")
+
+                elif not self.controller.has_faixa_etaria(commands[2]):
+                    print("Faixa etária inexistente.")
+
+                else:
+                    self.controller.registar_utente(commands[1])
+                    print("Utente registado com sucesso.")
+
+            # Resgistar Familia-----------------------------"RF NomeFamilia"
+            elif(commands[0] == "RF"):
+
+                if self.controller.has_familia(commands[1]):
+                    print("Família existente.")
+                else:
+                    self.controller.registar_familia(commands[1])
+                    print("Família registada com sucesso.")
+
+            # Associar utente a familia---------------------"AF Nome NomeFamiia"
+            elif(commands[0] == "AF"):
+                if not self.controller.has_utente(commands[1]):
+                    print("Utente inexistente.")
+
+                elif not self.controller.has_familia(commands[2]):
+                    print("Família inexistente.")
+
+                elif self.controller.get_utente(commands[1]).get_familia() != None:
+                    print("Utente pertence a família.")
+                else:
+                    self.controller.associar_utente_a_familia(
+                        commands[1], commands[2])
+
+            # Desassociar utende de familia------------------"DF Nome"
+            elif(commands[0] == "DF"):
+                if not self.controller.has_utente(commands[1]):
+                    print("Utente inexistente.")
+
+                elif not self.controller.get_utente(commands[1]).has_familia():
+                    print("Utente não pertence a família.")
+
+                else:
+                    self.controller.desassociar_utente_a_familia(commands[1])
+
+            # Listar Profissionais---------------------------"LP"
+            elif(commands[0] == "LP"):
+                pass
+
+            # Listar Utentes---------------------------------"LU"
+            elif(commands[0] == "LU"):
+                pass
+
+            # Listar Familias--------------------------------"LF"
+            elif(commands[0] == "LF"):
+                pass
+
+            # Mostrar Familia--------------------------------"MF NomeFamila"
+            elif(commands[0] == "MF"):
+                pass
+
+            # Marcar ciudados a utente-----------------------"MC Nome // Serviço // Categoria Nome Profissional"
+            elif(commands[0] == "MC"):
+                if not self.controller.has_utente(commands[1]):
+                    print("Utente inexistente.")
+
+                while True:
+                    subline = input()
+
+                    if subline == "":
+                        break
+
+                    inp = subline.split()
+                    if len(inp) == 1:
+                        if not self.controller.has_servico(inp[0]):
+                            print("Serviço inexistente.")
+
+                pass
+
+            # Cancelar cuidados a utente---------------------"CC Nome"
+            elif(commands[0] == "CC"):
+                pass
+
+            # Listar cuidados marcados a utente -------------"LCU Nome"
+            elif(commands[0] == "LCU"):
+                pass
+
+            # Listar cuidados marcados a Familia -------------"LCF NomeFamilia"
+            elif(commands[0] == "LCU"):
+                pass
+
+            # Listar serviços marcados a profissional----------"LSP Categoria NomeProfissional"
+            elif(commands[0] == "LSP"):
+                pass
+
+            # Listar marcações por tipo de serviço--------------"LMS Serviço"
+            elif(commands[0] == "LMS"):
+                pass
+
+            #Gravar---------------------------------------------"G[ NomeDoFicheiro]"
+            elif(commands[0] == "G"):
+                pass
+
+            #Ler-------------------------------------------------"L[ NomeDoFicheiro]"
+            elif(commands[0] == "L"):
+                pass
+            # ------------------------------------------------------------------------------------------------
             else:
-                controller.registar_profissional(commands[2])
-                print("Profissional registado com sucesso.")
-
-        # Resgistar Utente------------------------------"RU Nome FaixaEtária"
-        elif (commands[0] == "RU"):
-
-            if controller.has_utente(commands[1]):
-                print("Utente existente.")
-
-            elif not controller.has_faixa_etaria(commands[2]):
-                print("Faixa etária inexistente.")
-
-            else:
-                controller.registar_utente(commands[1])
-                print("Utente registado com sucesso.")
-
-        # Resgistar Familia-----------------------------"RF NomeFamilia"
-        elif(commands[0] == "RF"):
-
-            if controller.has_familia(commands[1]):
-                print("Família existente.")
-            else:
-                controller.registar_familia(commands[1])
-                print("Família registada com sucesso.")
-
-        # Associar utente a familia---------------------"AF Nome NomeFamiia"
-        elif(commands[0] == "AF"):
-            pass
-
-        # Desassociar utende de familia------------------"DF Nome"
-        elif(commands[0] == "DF"):
-            pass
-
-        # Listar Profissionais---------------------------"LP"
-        elif(commands[0] == "LP"):
-            pass
-
-        # Listar Utentes---------------------------------"LU"
-        elif(commands[0] == "LU"):
-            pass
-
-        # Listar Familias--------------------------------"LF"
-        elif(commands[0] == "LF"):
-            pass
-
-        # Mostrar Familia--------------------------------"MF NomeFamila"
-        elif(commands[0] == "MF"):
-            pass
-
-        # Marcar ciudados a utente-----------------------"MC Nome // Serviço // Categoria Nome Profissional"
-        elif(commands[0] == "MC"):
-            pass
-
-        # Cancelar cuidados a utente---------------------"CC Nome"
-        elif(commands[0] == "CC"):
-            pass
-
-        # Listar cuidados marcados a utente -------------"LCU Nome"
-        elif(commands[0] == "LCU"):
-            pass
-
-        # Listar cuidados marcados a Familia -------------"LCF NomeFamilia"
-        elif(commands[0] == "LCU"):
-            pass
-
-        # Listar serviços marcados a profissional----------"LSP Categoria NomeProfissional"
-        elif(commands[0] == "LSP"):
-            pass
-
-        # Listar marcações por tipo de serviço--------------"LMS Serviço"
-        elif(commands[0] == "LMS"):
-            pass
-
-        #Gravar---------------------------------------------"G[ NomeDoFicheiro]"
-        elif(commands[0] == "G"):
-            pass
-
-        #Ler-------------------------------------------------"L[ NomeDoFicheiro]"
-        elif(commands[0] == "L"):
-            pass
-        # ------------------------------------------------------------------------------------------------
-        else:
-            print("Instrução inválida")
+                print("Instrução inválida.")
